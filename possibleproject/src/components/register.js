cbb8ff5e1a6315bb617e0c266a85c70648742823
RickyBoy <rmsmp2003@gmail.com>
2025-03-29 14:55:08 +0000

    Made little ugly register frontend

---
+++ possibleproject/src/components/register.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "../App.css";
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div classname="register">
+      <div className="register_title">
+        <h1 classname="register_name_title">Poker Up!</h1>
+      </div>
+      <div className="user_interaction_register">
+        <div className="textfield_register_username">
+          <label htmlFor="Username">Username</label>
+          <input
+            type="text"
+            name="Username"
+            placeholder="Username"
+            maxLength="12"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="textfield_register_password">
+          <label htmlFor="Password">Password</label>
+          <input
+            type="password"
+            name="Password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
---
+++ possibleproject/src/pages/Register.js
@@ -0,0 +1,13 @@
+import Register from "../components/register";
+
+import React from "react";
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+};
+
+export default RegisterPage;
