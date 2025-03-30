a82101180b143a6ecf19a53ba87b2ccb22f82a8b
RickyBoy <rmsmp2003@gmail.com>
2025-03-30 04:03:51 +0100

    Neves é gay

--- possibleproject/src/components/register.js
+++ possibleproject/src/components/register.js
@@ -6,31 +6,36 @@ const Register = () => {
   const [password, setPassword] = useState("");
 
   return (
-    <div classname="register">
-      <div className="register_title">
-        <h1 classname="register_name_title">Poker Up!</h1>
-      </div>
-      <div className="user_interaction_register">
-        <div className="textfield_register_username">
-          <label htmlFor="Username">Username</label>
-          <input
-            type="text"
-            name="Username"
-            placeholder="Username"
-            maxLength="12"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+    <div className="register">
+      <div className="card_register">
+        <div className="register_title">
+          <h1 classname="register_name_title">Poker Up!</h1>
         </div>
-        <div className="textfield_register_password">
-          <label htmlFor="Password">Password</label>
-          <input
-            type="password"
-            name="Password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+        <div className="user_interaction_register">
+          <div className="textfield_register_username">
+            <label htmlFor="Username">Username</label>
+            <input
+              type="text"
+              name="Username"
+              placeholder="Username"
+              maxLength="12"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="textfield_register_password">
+            <label htmlFor="Password">Password</label>
+            <input
+              type="password"
+              name="Password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="reference_signin">
+            <p>If you already have an account,</p>
+          </div>
         </div>
       </div>
     </div>
